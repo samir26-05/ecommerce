65fe73f11c0a1dc67e5b2dc96ce2d52327712315
jesus afanador <Jesusarmado-2020@hotmail.com>
2023-08-11 13:57:44 -0500

    colocando localhost

--- backend-ecommerce/src/config.js
+++ backend-ecommerce/src/config.js
@@ -4,5 +4,5 @@ config()
 export const PORT= process.env.PORT || "3000"
 export const DATABASE_NAME= process.env.DATABASE_NAME || "ecommerce"
 export const DATABASE_USER= process.env.DATABASE_USER || "root"
-export const HOST = process.env.HOST || "192.168.12.43"
+export const HOST = process.env.HOST || "localhost"
 export const PASSWORD = process.env.DATABASE_PASSWORD || ""
