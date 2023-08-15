5992e1d361c4c3ccc3effa8250a61d651007bb18
Edelahoz26 <edelahoz@ulearning.com.co>
2023-08-15 09:17:20 -0500

    Funcionamiento Registro

--- frontend-ecommerce/src/pages/Login/Login.jsx
+++ frontend-ecommerce/src/pages/Login/Login.jsx
@@ -9,6 +9,7 @@ import { FlexDirCol, FlexRow } from "../../components/StyledMain";
 import TextField from "@mui/material/TextField";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import GoogleIcon from "@mui/icons-material/Google";
+import axios from "axios";
 
 export default function LoginDrawer() {
   const [haveAccount, setHaveAccount] = useState(true);
@@ -40,20 +41,20 @@ export default function LoginDrawer() {
       role="presentation"
     >
       <List style={{ paddingTop: "0" }}>
-          <FlexRow style={{ marginBottom: "1rem", fontSize:"1.2rem" }}>
-            <Login_Register
-              onClick={() => setHaveAccount(true)}
-              Page={haveAccount}
-            >
-              Iniciar Sesión
-            </Login_Register>
-            <Login_Register
-              onClick={() => setHaveAccount(false)}
-              Page={haveAccount}
-            >
-              Registarse
-            </Login_Register>
-          </FlexRow>
+        <FlexRow style={{ marginBottom: "1rem", fontSize: "1.2rem" }}>
+          <Login_Register
+            onClick={() => setHaveAccount(true)}
+            Page={haveAccount}
+          >
+            Iniciar Sesión
+          </Login_Register>
+          <Login_Register
+            onClick={() => setHaveAccount(false)}
+            Page={haveAccount}
+          >
+            Registarse
+          </Login_Register>
+        </FlexRow>
         {haveAccount ? <Login /> : <Register />}
       </List>
     </Box>
@@ -78,33 +79,90 @@ export default function LoginDrawer() {
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-function BasicTextFields({ Placeholder }) {
+function BasicTextFields({ Placeholder, onChange, value }) {
   return (
     <Box
       component="form"
       sx={{
-        "& > :not(style)": { m: 1, width: "35ch" }, 
+        "& > :not(style)": { m: 1, width: "35ch" },
       }}
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label={Placeholder} variant="outlined" />
+      <TextField
+        id={Placeholder}
+        label={Placeholder}
+        onChange={onChange}
+        value={value}
+        variant="outlined"
+      />
     </Box>
   );
 }
 
+
+
 const Login = () => {
+  
+  
+
+  const [users, setUsers] = useState({
+    user: '',
+    password: '',
+  });
+  
+  const handleInputJoin = (campo, valor) => {
+    users((datosPrevios) => ({ ...datosPrevios, [campo]: valor }));
+  };
+
+  const conx = async(event) => { 
+    event.preventDefault();
+    try {
+      const response = await axios.get("http://localhost:3000/login",{
+          ...users
+      });
+      console.log(response);
+  } catch (error) {
+      console.log(error);
+  }
+   }
+  
   return (
     <FlexDirCol style={{ gap: ".5rem" }}>
+
+      <form onSubmit={conx}>
+
+      <input
+          className="controls"
+          type="text"
+          name="nombre"
+          placeholder="Ingrese su correo"
+          required
+          onChange={(event) => {
+            handleInputJoin("email",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="nombre"
+          placeholder="Ingrese su contraseña"
+          required
+          onChange={(event) => {
+            handleInputJoin("email",event.target.value);
+          }}
+        />
+      </form>
+      
       <BasicTextFields Placeholder={"E-mail"} />
       <BasicTextFields Placeholder={"Contraseña"} />
       <FlexRow style={{ justifyContent: "space-between" }}>
         <Span>
-          <Checkbox {...label} style={{color:"black"}}/> Recordar contraseña
+          <Checkbox {...label} style={{ color: "black" }} /> Recordar contraseña
         </Span>
         <Span>¿Olvidaste tu contraseña?</Span>
       </FlexRow>
-      <Btn>Iniciar Sesión</Btn>
+      <Btn type="submit" value="registrar">Iniciar Sesión</Btn>
       <FlexDirCol style={{ gap: ".5rem" }}>
         <SignInBtn bgcolor="#003aaf">
           <FacebookIcon /> Continuar con Facebook
@@ -117,25 +175,102 @@ const Login = () => {
   );
 };
 
-const Register = () => {
+export const Register = () => {
+
+  const [Propiedaddes,setpropieda] = useState({
+    email: "",
+    password_hash: "",
+    first_name: "",
+    last_name: "",
+    address: "",
+    phone_number: ""
+  })
+  const handleInputChange = (campo, valor) => {
+    setpropieda((datosPrevios) => ({ ...datosPrevios, [campo]: valor }));
+  };
+  console.log(Propiedaddes)
+  
+  const add = async (event) =>{
+    event.preventDefault();
+    try {
+        const response = await axios.post("http://localhost:3000/users/register",{
+            ...Propiedaddes
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
   return (
-    <FlexDirCol style={{ gap: ".2rem" }}>
-      <BasicTextFields Placeholder={"Nombres"} />
-      <BasicTextFields Placeholder={"Apellidos"} />
-      <BasicTextFields Placeholder={"E-mail"} />
-      <BasicTextFields Placeholder={"Dirección"} />
-      <BasicTextFields Placeholder={"Teléfono"} type="number" />
-      <BasicTextFields Placeholder={"Contraseña"} type="password" />
-      <BasicTextFields Placeholder={"Confirmar Contraseña"} type="password" />
-      <Btn>Crear Cuenta</Btn>
-      <FlexDirCol style={{ gap: ".5rem" }}>
-        <SignInBtn bgcolor="#003aaf">
-          <FacebookIcon /> Continuar con Facebook
-        </SignInBtn>
-        <SignInBtn bgcolor="#fff">
-          <GoogleIcon /> Continuar con Google
-        </SignInBtn>
-      </FlexDirCol>
-    </FlexDirCol>
+    <form onSubmit={add} >
+        <input
+          className="controls"
+          type="text"
+          name="nombre"
+          placeholder="Ingrese su correo"
+          required
+          onChange={(event) => {
+            handleInputChange("email",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="password"
+          name="email"
+          id="email"
+          placeholder="Ingrese su contraseña"
+          required
+          onChange={(event) => {
+            handleInputChange("password_hash",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="contraseña"
+          id="contraseña"
+          placeholder="Ingrese su nombre"
+          required
+          onChange={(event) => {
+            handleInputChange("first_name",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="contraseña"
+          id="contraseña"
+          placeholder="ingrese su apellido"
+          required
+          onChange={(event) => {
+            handleInputChange("last_name",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="contraseña"
+          id="contraseña"
+          placeholder="Ingrese su dirrecion"
+          required
+          onChange={(event) => {
+            handleInputChange("address",event.target.value);
+          }}
+        />
+        <input
+          className="controls"
+          type="text"
+          name="contraseña"
+          id="contraseña"
+          placeholder="Ingrse su numero de telefono"
+          required
+          onChange={(event) => {
+            handleInputChange("phone_number",event.target.value);
+          }}
+        />
+        <button type="submit" value="registrar" />
+
+      </form>
   );
 };
