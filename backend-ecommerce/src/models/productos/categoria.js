1ebfb5faae954b431887b146f69dc3869bc21e22
jesus afanador <Jesusarmado-2020@hotmail.com>
2023-08-24 13:21:33 -0500

    corrigiendo el nombre de users a user y haci con las mayoria de tablas

--- backend-ecommerce/src/models/productos/categoria.js
+++ backend-ecommerce/src/models/productos/categoria.js
@@ -1,14 +1,17 @@
-import { DataTypes } from "sequelize";
+import { DataTypes} from "sequelize";
 import { sequelize } from "../../database.js";
-export const categoria = sequelize.define("categorys", {
-  Category_id: {
-    type: DataTypes.INTEGER,
-    PrimaryKey: true,
-    allowNull: false,
-    autoincrement: true,
-  },
-  Category: {
-    type: DataTypes.STRING(20),
-    allowNull: false,
-  }
-});
+export const category = sequelize.define("category",{
+  category_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoincrement: true,
+        allowNull: false,
+    },
+    category:{
+        type: DataTypes.STRING(30),
+        allowNull: false,
+    }
+},{
+  tableName: "category"
+})
+
