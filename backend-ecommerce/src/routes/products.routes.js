4de9a3a10993e98b4999383a7212f7a50f89c46b
samir26-05 <saorozco2605@gmail.com>
2023-08-30 13:55:25 -0500

    cambios

--- backend-ecommerce/src/routes/products.routes.js
+++ backend-ecommerce/src/routes/products.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import * as ProductsControlles from "../controllers/products/products.controllers.js"
 const router = Router()
 
 // listar productos 
-router.post('/create',)
-router.get('/',)
+router.post('/create', )
+router.get('/get', ProductsControlles.getCategory)
 
 export default router
