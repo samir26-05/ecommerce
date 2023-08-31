9cc52262d6e78d8eda5163b3b7baa5c1ff4b2f32
samir26-05 <saorozco2605@gmail.com>
2023-08-31 17:06:00 -0500

    add products

--- frontend-ecommerce/src/components/forms/products/CreateProducts.jsx
+++ frontend-ecommerce/src/components/forms/products/CreateProducts.jsx
@@ -103,6 +103,33 @@ export default function FormProducts() {
     }));
   };
 
+
+
+  const validateName = (nombre) => {
+    return nombre.trim() !== '';
+  };
+  const validateSize = (talla) => {
+    return talla !== 0 && talla !== 'Selecciona una opción';
+  };
+  const validateDescription = (descripcion) => {
+    return descripcion.trim() !== '';
+  };
+  const validateBrand = (marca) => {
+    return marca !== 0 && marca !== 'Selecciona una opción';
+  };
+  const validatePrice = (precio) => {
+    return parseFloat(precio) > 0;
+  };
+  const validateCategory = (categoria) => {
+    return categoria !== 0 && categoria !== 'Selecciona una opción';
+  };
+  const validateSection = (seccion) => {
+    return seccion !== 0 && seccion !== 'Selecciona una opción';
+  };
+  const validateStock = (cantidad) => {
+    return parseInt(cantidad) >= 0;
+  };
+
   const validateForm = () => {
     const newErrors = {
       nombre: !validateName(newProduct.name) ? 'Ingrese un nombre del producto.' : '',
@@ -119,58 +146,28 @@ export default function FormProducts() {
     return Object.values(newErrors).every((error) => error === '');
   };
 
-  const validateName = (nombre) => {
-    return nombre.trim() !== '';
-  };
 
-  const validateSize = (talla) => {
-    return talla !== 0 && talla !== 'Selecciona una opción';
-  };
-
-  const validateDescription = (descripcion) => {
-    return descripcion.trim() !== '';
-  };
-
-  const validateBrand = (marca) => {
-    return marca !== 0 && marca !== 'Selecciona una opción';
-  };
-
-  const validatePrice = (precio) => {
-    return parseFloat(precio) > 0;
-  };
-
-  const validateCategory = (categoria) => {
-    return categoria !== 0 && categoria !== 'Selecciona una opción';
-  };
-
-  const validateSection = (seccion) => {
-    return seccion !== 0  && seccion !== 'Selecciona una opción';
-  };
-
-  const validateStock = (cantidad) => {
-    return parseInt(cantidad) >= 0;
-  };
-
-
+  const formData = new FormData();
+  const [File, setFile] = useState();
+  formData.append("data", JSON.stringify(newProduct));
+  formData.append("file", File);
+  console.log(File)
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     if (!validateForm()) {
-      return;
+      return console.log("estoy entrando aqui")
     }
-  
+
     try {
-      const response = await axios.post(
-        'http://localhost:3000/product/create', 
-        newProduct,
-        {
-          headers: {
-            accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJzYW9yb3pjbzI2MDUiLCJpYXQiOjE2OTM0ODkwNjAsImV4cCI6MTY5MzQ5MjY2MH0.2p5fQpQXnfGgnDMEpAxQ3fOFZEA520MhwjpMU2OTWgw",
-          },
+      const response = await axios.post('http://localhost:3000/product/create', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'accessToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJzYW9yb3pjbzI2MDUiLCJpYXQiOjE2OTM1MDM5ODksImV4cCI6MTY5MzUwNzU4OX0.c6XdjZawdn4_i5O_bevEtpOQzTwnx-bFYeLxJ--Zf6o'
         }
-      );
-  
+      })
+
       if (response.status === 200) {
         navigate('/user');
         alert(response.data.message);
@@ -187,150 +184,161 @@ export default function FormProducts() {
 
   return (
     <Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '25ch', top: '20px' } }} noValidate autoComplete="off" >
-    
-        <div>
-          <TextField
-            id="outlined-select-currency"
-            label="Seccion"
-            select
-            value={newProduct.id_section}
-            onChange={(event) => handleInputChange("id_section", event.target.value)}
-            required
-            error={Boolean(errors.seccion)}
-            helperText={errors.seccion}
-          >
-            {sections.map((seccion) => (
-              <MenuItem  key={seccion.id_section} value={seccion.id_section}>
-                {seccion.section}
-              </MenuItem >
-            ))}
-          </TextField>
 
-          <TextField
-            id="outlined-select-currency"
-            label="Categoria"
-            select
-            value={newProduct.category_id}
-            onChange={(event) => handleInputChange("category_id", event.target.value)}
-            required
-            error={Boolean(errors.categoria)}
-            helperText={errors.categoria}
-          >
-            {categories.map((categoria) => (
-              <MenuItem key={categoria.category_id} value={categoria.category_id}>
-                {categoria.category}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            type="text"
-            label="Nombre"
-            value={newProduct.name}
-            onChange={(event) => handleInputChange("name", event.target.value)}
-            required
-            error={Boolean(errors.nombre)}
-            helperText={errors.nombre}
-          />
-          <TextField
-            type="text"
-            label="Descripción"
-            value={newProduct.description}
-            onChange={(event) => handleInputChange("description", event.target.value)}
-            required
-            error={Boolean(errors.descripcion)}
-            helperText={errors.descripcion}
-          />
-        </div>
+      <div>
+        <TextField
+          id="outlined-select-currency"
+          label="Seccion"
+          select
+          value={newProduct.id_section}
+          onChange={(event) => handleInputChange("id_section", event.target.value)}
+          required
+          error={Boolean(errors.seccion)}
+          helperText={errors.seccion}
+        >
+          {sections.map((seccion) => (
+            <MenuItem key={seccion.id_section} value={seccion.id_section}>
+              {seccion.section}
+            </MenuItem >
+          ))}
+        </TextField>
 
-        <div>
-          <TextField
-            className="controls"
-            type="number"
-            name="precio"
-            label="Precio"
-            required
-            error={Boolean(errors.precio)}
-            helperText={errors.precio}
-            onChange={(event) => {
-              handleInputChange("price", event.target.value);
-            }}
-            InputProps={{
-              inputProps: { min: 0 }, // Evitar valores negativos
-            }}
-          />
+        <TextField
+          id="outlined-select-currency"
+          label="Categoria"
+          select
+          value={newProduct.category_id}
+          onChange={(event) => handleInputChange("category_id", event.target.value)}
+          required
+          error={Boolean(errors.categoria)}
+          helperText={errors.categoria}
+        >
+          {categories.map((categoria) => (
+            <MenuItem key={categoria.category_id} value={categoria.category_id}>
+              {categoria.category}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          type="text"
+          label="Nombre"
+          value={newProduct.name}
+          onChange={(event) => handleInputChange("name", event.target.value)}
+          required
+          error={Boolean(errors.nombre)}
+          helperText={errors.nombre}
+        />
+        <TextField
+          type="text"
+          label="Descripción"
+          value={newProduct.description}
+          onChange={(event) => handleInputChange("description", event.target.value)}
+          required
+          error={Boolean(errors.descripcion)}
+          helperText={errors.descripcion}
+        />
+      </div>
 
-          
-          <TextField
-            type="text"
-            label="Talla"
-            select
-            value={newProduct.sizes_id}
-            onChange={(event) => handleInputChange("sizes_id", event.target.value)}
-            required
-            error={Boolean(errors.categoria)}
-            helperText={errors.categoria}
-          >
-            {sizes.map((talla) => (
-              <MenuItem key={talla.sizes_id} value={talla.sizes_id}>
-                {talla.size}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          <TextField
-            type="text"
-            label="Marca"
-            select
-            value={newProduct.id_brands}
-            onChange={(event) => handleInputChange("id_brands", event.target.value)}
-            required
-            error={Boolean(errors.categoria)}
-            helperText={errors.categoria}
-          >
-            {brands.map((marca) => (
-              <MenuItem key={marca.id_brands} value={marca.id_brands}>
-                {marca.brand}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          <TextField
-            id="outlined-multiline-flexible"
-            type="number"
-            label="Cantidad*"
-            value={newProduct.stock}
-            onChange={(event) => handleInputChange("stock", event.target.value)}
-            required
-            error={Boolean(errors.cantidad)}
-            helperText={errors.cantidad}
-            InputProps={{
-              inputProps: { min: 0 }, // Evitar valores negativos
-            }}
-          />
-        </div>
+      <div>
+        <TextField
+          className="controls"
+          type="number"
+          name="precio"
+          label="Precio"
+          required
+          error={Boolean(errors.precio)}
+          helperText={errors.precio}
+          onChange={(event) => {
+            handleInputChange("price", event.target.value);
+          }}
+          InputProps={{
+            inputProps: { min: 0 }, // Evitar valores negativos
+          }}
+        />
 
 
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <input accept="image/*" style={{ display: 'none' }} id="image-input" type="file" onChange={handleImageChange} />
-          <label htmlFor="image-input" style={{ color: "black" }}>
-            <Button variant="outline" className='whithoutOutline' component="span" style={{ backgroundColor: "#ffffff", marginTop: "45px" }}>
-              Cargar imagen <PiUploadThin />
-            </Button>
-          </label>
+        <TextField
+          type="text"
+          label="Talla"
+          select
+          value={newProduct.sizes_id}
+          onChange={(event) => handleInputChange("sizes_id", event.target.value)}
+          required
+          error={Boolean(errors.categoria)}
+          helperText={errors.categoria}
+        >
+          {sizes.map((talla) => (
+            <MenuItem key={talla.sizes_id} value={talla.sizes_id}>
+              {talla.size}
+            </MenuItem>
+          ))}
+        </TextField>
 
-        </div>
+        <TextField
+          type="text"
+          label="Marca"
+          select
+          value={newProduct.id_brands}
+          onChange={(event) => handleInputChange("id_brands", event.target.value)}
+          required
+          error={Boolean(errors.categoria)}
+          helperText={errors.categoria}
+        >
+          {brands.map((marca) => (
+            <MenuItem key={marca.id_brands} value={marca.id_brands}>
+              {marca.brand}
+            </MenuItem>
+          ))}
+        </TextField>
 
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%", border: "solid 1px black" }}>
-          {selectedImage && (
-            <div style={{ width: "25%", height: "25%", marginTop: 10 }}>
-              <img src={selectedImage} alt="Imagen seleccionada" />
-            </div>
-          )}
-        </div>
-        <div>
-          <Button variant="contained" className='whithoutOutline' style={{ backgroundColor: "black", marginTop: 45 }} type="submit" value="crear" onClick={handleSubmit}>CREAR PRODUCTO</Button>
-        </div>
-     
+        <TextField
+          id="outlined-multiline-flexible"
+          type="number"
+          label="Cantidad*"
+          value={newProduct.stock}
+          onChange={(event) => handleInputChange("stock", event.target.value)}
+          required
+          error={Boolean(errors.cantidad)}
+          helperText={errors.cantidad}
+          InputProps={{
+            inputProps: { min: 0 }, // Evitar valores negativos
+          }}
+        />
+      </div>
+
+
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <input accept="image/*" style={{ display: 'none' }} id="image-input" type="file" onChange={handleImageChange} />
+        <input
+          className="controls"
+          type="file"
+          name="contraseña"
+          id="contraseña"
+          placeholder="Ingrese su imagen"
+          required
+          onChange={(event) => {
+            setFile(event.target.files[0]);
+          }}
+        />
+        <label htmlFor="image-input" style={{ color: "black" }}>
+          <Button variant="outline" className='whithoutOutline' component="span" style={{ backgroundColor: "#ffffff", marginTop: "45px" }}>
+            Cargar imagen <PiUploadThin />
+          </Button>
+        </label>
+
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%", border: "solid 1px black" }}>
+        {selectedImage && (
+          <div style={{ width: "25%", height: "25%", marginTop: 10 }}>
+            <img src={selectedImage} alt="Imagen seleccionada" />
+          </div>
+        )}
+      </div>
+      <div>
+        <Button variant="contained" className='whithoutOutline' style={{ backgroundColor: "black", marginTop: 45 }} type="submit" value="crear" onClick={handleSubmit}>CREAR PRODUCTO</Button>
+      </div>
+
     </Box >
 
   );
