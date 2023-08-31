39cb5b421a368505149e868bf3ca182b93823da6
samir26-05 <saorozco2605@gmail.com>
2023-08-31 17:27:37 -0500

    cambios add products

--- frontend-ecommerce/src/components/forms/products/CreateProducts.jsx
+++ frontend-ecommerce/src/components/forms/products/CreateProducts.jsx
@@ -1,21 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 /* MATERIAL UI */
-import Box from '@mui/material/Box';
-import { Button, MenuItem, TextField } from '@mui/material';
-import { PiUploadThin } from 'react-icons/pi'
+import Box from "@mui/material/Box";
+import { Button, MenuItem, TextField } from "@mui/material";
+import { PiUploadThin } from "react-icons/pi";
 /* STYLES */
-import '../../Layout/header/header.css'
+import "../../Layout/header/header.css";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function FormProducts() {
-
   const [selectedImage, setSelectedImage] = useState(null);
   const [sections, setSections] = useState([]);
   const [categories, setCategories] = useState([]);
   const [sizes, setSizes] = useState([]);
   const [brands, setBrands] = useState([]);
-
 
   const handleImageChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -29,55 +27,58 @@ export default function FormProducts() {
   useEffect(() => {
     async function fetchSections() {
       try {
-        const response = await axios.get('http://localhost:3000/product/Section');
+        const response = await axios.get(
+          "http://localhost:3000/product/Section"
+        );
         setSections(response.data);
       } catch (error) {
-        console.error('Error al obtener las secciones:', error);
+        console.error("Error al obtener las secciones:", error);
       }
     }
 
     async function fetchCategories() {
       try {
-        const response = await axios.get('http://localhost:3000/product/category');
+        const response = await axios.get(
+          "http://localhost:3000/product/category"
+        );
         setCategories(response.data);
       } catch (error) {
-        console.error('Error al obtener categorías:', error);
+        console.error("Error al obtener categorías:", error);
       }
     }
 
     async function fetchSizes() {
       try {
-        const response = await axios.get('http://localhost:3000/product/size');
+        const response = await axios.get("http://localhost:3000/product/size");
         setSizes(response.data);
       } catch (error) {
-        console.error('Error al obtener las tallas:', error);
+        console.error("Error al obtener las tallas:", error);
       }
     }
 
     async function fetchBrands() {
       try {
-        const response = await axios.get('http://localhost:3000/product/brand');
+        const response = await axios.get("http://localhost:3000/product/brand");
         setBrands(response.data);
       } catch (error) {
-        console.error('Error al obtener las marcas:', error);
+        console.error("Error al obtener las marcas:", error);
       }
     }
 
     fetchSections();
     fetchCategories();
     fetchSizes();
-    fetchBrands()
+    fetchBrands();
   }, []);
-
 
   /* ------------------------------------------------------------------------------------------------------------------- */
 
   let navigate = useNavigate();
 
   const [newProduct, setNewProduct] = useState({
-    name: '',
+    name: "",
     sizes_id: 0,
-    description: '',
+    description: "",
     id_brands: 0,
     price: 0,
     category_id: 0,
@@ -86,14 +87,14 @@ export default function FormProducts() {
   });
 
   const [errors, setErrors] = useState({
-    nombre: '',
-    talla: '',
-    descripcion: '',
-    marca: '',
-    precio: '',
-    categoria: '',
-    seccion: '',
-    cantidad: '',
+    nombre: "",
+    talla: "",
+    descripcion: "",
+    marca: "",
+    precio: "",
+    categoria: "",
+    seccion: "",
+    cantidad: "",
   });
 
   const handleInputChange = (campo, valor) => {
@@ -103,28 +104,26 @@ export default function FormProducts() {
     }));
   };
 
-
-
   const validateName = (nombre) => {
-    return nombre.trim() !== '';
+    return nombre.trim() !== "";
   };
   const validateSize = (talla) => {
-    return talla !== 0 && talla !== 'Selecciona una opción';
+    return talla !== 0 && talla !== "Selecciona una opción";
   };
   const validateDescription = (descripcion) => {
-    return descripcion.trim() !== '';
+    return descripcion.trim() !== "";
   };
   const validateBrand = (marca) => {
-    return marca !== 0 && marca !== 'Selecciona una opción';
+    return marca !== 0 && marca !== "Selecciona una opción";
   };
   const validatePrice = (precio) => {
     return parseFloat(precio) > 0;
   };
   const validateCategory = (categoria) => {
-    return categoria !== 0 && categoria !== 'Selecciona una opción';
+    return categoria !== 0 && categoria !== "Selecciona una opción";
   };
   const validateSection = (seccion) => {
-    return seccion !== 0 && seccion !== 'Selecciona una opción';
+    return seccion !== 0 && seccion !== "Selecciona una opción";
   };
   const validateStock = (cantidad) => {
     return parseInt(cantidad) >= 0;
@@ -132,66 +131,91 @@ export default function FormProducts() {
 
   const validateForm = () => {
     const newErrors = {
-      nombre: !validateName(newProduct.name) ? 'Ingrese un nombre del producto.' : '',
-      talla: !validateSize(newProduct.sizes_id) ? 'Ingrese la talla del producto.' : '',
-      descripcion: !validateDescription(newProduct.description) ? 'Ingrese una descripción para el producto.' : '',
-      marca: !validateBrand(newProduct.id_brands) ? 'Ingrese la marca del producto.' : '',
-      precio: !validatePrice(newProduct.price) ? 'Ingrese el precio del producto.' : '',
-      categoria: !validateCategory(newProduct.category_id) ? 'Ingrese la categoría del producto.' : '',
-      seccion: !validateSection(newProduct.id_section) ? 'Ingrese la sección a la que pertenece el producto.' : '',
-      cantidad: !validateStock(newProduct.stock) ? 'Ingrese la cantidad de este producto.' : '',
+      nombre: !validateName(newProduct.name)
+        ? "Ingrese un nombre del producto."
+        : "",
+      talla: !validateSize(newProduct.sizes_id)
+        ? "Ingrese la talla del producto."
+        : "",
+      descripcion: !validateDescription(newProduct.description)
+        ? "Ingrese una descripción para el producto."
+        : "",
+      marca: !validateBrand(newProduct.id_brands)
+        ? "Ingrese la marca del producto."
+        : "",
+      precio: !validatePrice(newProduct.price)
+        ? "Ingrese el precio del producto."
+        : "",
+      categoria: !validateCategory(newProduct.category_id)
+        ? "Ingrese la categoría del producto."
+        : "",
+      seccion: !validateSection(newProduct.id_section)
+        ? "Ingrese la sección a la que pertenece el producto."
+        : "",
+      cantidad: !validateStock(newProduct.stock)
+        ? "Ingrese la cantidad de este producto."
+        : "",
     };
 
     setErrors(newErrors);
-    return Object.values(newErrors).every((error) => error === '');
+    return Object.values(newErrors).every((error) => error === "");
   };
-
 
   const formData = new FormData();
   const [File, setFile] = useState();
   formData.append("data", JSON.stringify(newProduct));
   formData.append("file", File);
-  console.log(File)
+  console.log(File);
+  console.log(888, formData);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!validateForm()) {
-      return console.log("estoy entrando aqui")
+      return console.log("estoy entrando aqui");
     }
 
     try {
-      const response = await axios.post('http://localhost:3000/product/create', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'accessToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJzYW9yb3pjbzI2MDUiLCJpYXQiOjE2OTM1MDM5ODksImV4cCI6MTY5MzUwNzU4OX0.c6XdjZawdn4_i5O_bevEtpOQzTwnx-bFYeLxJ--Zf6o'
+      const response = await axios.post(
+        "http://localhost:3000/product/create",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            accessToken:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJzYW9yb3pjbzI2MDUiLCJpYXQiOjE2OTM1MDM5ODksImV4cCI6MTY5MzUwNzU4OX0.c6XdjZawdn4_i5O_bevEtpOQzTwnx-bFYeLxJ--Zf6o",
+          },
         }
-      })
+      );
 
       if (response.status === 200) {
-        navigate('/user');
+        navigate("/user");
         alert(response.data.message);
       }
     } catch (error) {
-      console.error('Error en el registro:', error);
-      alert('Ocurrió un error durante el registro. Por favor, inténtelo de nuevo más tarde.');
+      console.error("Error en el registro:", error);
+      alert(
+        "Ocurrió un error durante el registro. Por favor, inténtelo de nuevo más tarde."
+      );
     }
-
-
   };
 
-
-
   return (
-    <Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '25ch', top: '20px' } }} noValidate autoComplete="off" >
-
+    <Box
+      component="form"
+      sx={{ "& .MuiTextField-root": { m: 1, width: "25ch", top: "20px" } }}
+      noValidate
+      autoComplete="off"
+    >
       <div>
         <TextField
           id="outlined-select-currency"
           label="Seccion"
           select
           value={newProduct.id_section}
-          onChange={(event) => handleInputChange("id_section", event.target.value)}
+          onChange={(event) =>
+            handleInputChange("id_section", event.target.value)
+          }
           required
           error={Boolean(errors.seccion)}
           helperText={errors.seccion}
@@ -199,7 +223,7 @@ export default function FormProducts() {
           {sections.map((seccion) => (
             <MenuItem key={seccion.id_section} value={seccion.id_section}>
               {seccion.section}
-            </MenuItem >
+            </MenuItem>
           ))}
         </TextField>
 
@@ -208,7 +232,9 @@ export default function FormProducts() {
           label="Categoria"
           select
           value={newProduct.category_id}
-          onChange={(event) => handleInputChange("category_id", event.target.value)}
+          onChange={(event) =>
+            handleInputChange("category_id", event.target.value)
+          }
           required
           error={Boolean(errors.categoria)}
           helperText={errors.categoria}
@@ -232,7 +258,9 @@ export default function FormProducts() {
           type="text"
           label="Descripción"
           value={newProduct.description}
-          onChange={(event) => handleInputChange("description", event.target.value)}
+          onChange={(event) =>
+            handleInputChange("description", event.target.value)
+          }
           required
           error={Boolean(errors.descripcion)}
           helperText={errors.descripcion}
@@ -256,13 +284,14 @@ export default function FormProducts() {
           }}
         />
 
-
         <TextField
           type="text"
           label="Talla"
           select
           value={newProduct.sizes_id}
-          onChange={(event) => handleInputChange("sizes_id", event.target.value)}
+          onChange={(event) =>
+            handleInputChange("sizes_id", event.target.value)
+          }
           required
           error={Boolean(errors.categoria)}
           helperText={errors.categoria}
@@ -279,7 +308,9 @@ export default function FormProducts() {
           label="Marca"
           select
           value={newProduct.id_brands}
-          onChange={(event) => handleInputChange("id_brands", event.target.value)}
+          onChange={(event) =>
+            handleInputChange("id_brands", event.target.value)
+          }
           required
           error={Boolean(errors.categoria)}
           helperText={errors.categoria}
@@ -306,9 +337,14 @@ export default function FormProducts() {
         />
       </div>
 
-
       <div style={{ display: "flex", flexDirection: "row" }}>
-        <input accept="image/*" style={{ display: 'none' }} id="image-input" type="file" onChange={handleImageChange} />
+        <input
+          accept="image/*"
+          style={{ display: "none" }}
+          id="image-input"
+          type="file"
+          onChange={handleImageChange}
+        />
         <input
           className="controls"
           type="file"
@@ -321,14 +357,26 @@ export default function FormProducts() {
           }}
         />
         <label htmlFor="image-input" style={{ color: "black" }}>
-          <Button variant="outline" className='whithoutOutline' component="span" style={{ backgroundColor: "#ffffff", marginTop: "45px" }}>
+          <Button
+            variant="outline"
+            className="whithoutOutline"
+            component="span"
+            style={{ backgroundColor: "#ffffff", marginTop: "45px" }}
+          >
             Cargar imagen <PiUploadThin />
           </Button>
         </label>
-
       </div>
 
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%", border: "solid 1px black" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          border: "solid 1px black",
+        }}
+      >
         {selectedImage && (
           <div style={{ width: "25%", height: "25%", marginTop: 10 }}>
             <img src={selectedImage} alt="Imagen seleccionada" />
@@ -336,10 +384,17 @@ export default function FormProducts() {
         )}
       </div>
       <div>
-        <Button variant="contained" className='whithoutOutline' style={{ backgroundColor: "black", marginTop: 45 }} type="submit" value="crear" onClick={handleSubmit}>CREAR PRODUCTO</Button>
+        <Button
+          variant="contained"
+          className="whithoutOutline"
+          style={{ backgroundColor: "black", marginTop: 45 }}
+          type="submit"
+          value="crear"
+          onClick={handleSubmit}
+        >
+          CREAR PRODUCTO
+        </Button>
       </div>
-
-    </Box >
-
+    </Box>
   );
 }
