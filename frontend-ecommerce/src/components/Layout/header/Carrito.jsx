e1c67ed64b8a48f94707653430dea449208cb0bb
Jose Rueda <ruedazambrano@gmail.com>
2023-08-31 10:18:06 -0500

    Botton añadir en carrito styled listo

--- frontend-ecommerce/src/components/Layout/header/Carrito.jsx
+++ frontend-ecommerce/src/components/Layout/header/Carrito.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import "./car.css";
 import cesta from "../../../assets/Img/cesta.png";
 import { useCart } from "../body/products/CardContext";
-
+import { MdAdd } from "react-icons/Md";
 export const Carrito = ({ hover, pageUsed, pagePayment, color }) => {
   const [active, setActive] = useState(false);
 
@@ -74,7 +74,7 @@ export const Carrito = ({ hover, pageUsed, pagePayment, color }) => {
                         <img
                           src={product.img}
                           alt=""
-                          style={{ width: 120, height: 120 }}
+                          style={{ width: 100, height: 120 }}
                         />
                         <span className="cantidad-producto-carrito">
                           {product.quantity}
@@ -90,22 +90,29 @@ export const Carrito = ({ hover, pageUsed, pagePayment, color }) => {
                         </div>
                       </div>
                     </Link>
-                    <button className="btndele">
-                      <svg
-                        viewBox="0 0 15 17.5"
-                        height="17.5"
-                        width="15"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="icon"
-                        onClick={() => onDeleteProduct(product)}
-                      >
-                        <path
-                          transform="translate(-2.5 -1.25)"
-                          d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
-                          id="Fill"
-                        ></path>
-                      </svg>
-                    </button>
+                    <div className="Infoon-product containerButtons">
+                    <button className="btnAdd">
+                      <MdAdd className="iconAdd"
+                      id="Fill"
+                      size={"18px"}/>
+                      </button>
+                      <button className="btndele">
+                        <svg
+                          viewBox="0 0 15 17.5"
+                          height="17.5"
+                          width="15"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon"
+                          onClick={() => onDeleteProduct(product)}
+                        >
+                          <path
+                            transform="translate(-2.5 -1.25)"
+                            d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
+                            id="Fill"
+                          ></path>
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 ))}
               </div>
