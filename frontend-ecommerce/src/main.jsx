751ef4de44a47ddcc7e2b8ac82cfbe505fef8255
Edelahoz26 <edelahoz@ulearning.com.co>
2023-09-01 17:31:38 -0500

    ultimo cambio login y diseño

--- frontend-ecommerce/src/main.jsx
+++ frontend-ecommerce/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { CartProvider } from "./components/Layout/body/products/CardContext.jsx";
 import "./index.css";
 import "./normalize.css";
-
+import "./pages/Login/style.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
