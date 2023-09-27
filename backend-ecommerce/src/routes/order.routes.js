77185f5ac450a113b41e57c2a73495236d3da66f
jesus afanador <Jesusarmado-2020@hotmail.com>
2023-09-27 15:53:43 -0500

    buscar usuario segun su estado

--- backend-ecommerce/src/routes/order.routes.js
+++ backend-ecommerce/src/routes/order.routes.js
@@ -8,4 +8,5 @@ router.post('/create',Jwt.validatetoken,Order.CreateOrder)
 router.get('/name',Jwt.validatetoken,Order.GetOrderUser)
 router.get('/chekout',Jwt.validatetoken,Order.CheckoutPago)
 router.get('/status/:name',Jwt.validatetoken,Order.GetOrderStatus)
+router.get('/user/:name/statu',Jwt.validatetoken,Order.GetOrderStatusUser)
 export default router
