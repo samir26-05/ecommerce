184578656c163d33c7520d5ccece2496d7505a75
Edelahoz26 <edelahoz@ulearning.com.co>
2023-10-05 09:23:14 -0500

    cambios para actualizacion de datos en mensajes de error

--- frontend-ecommerce/src/components/navs/infoUser/UpdatePass.jsx
+++ frontend-ecommerce/src/components/navs/infoUser/UpdatePass.jsx
@@ -48,7 +48,7 @@ export default function UpdatePass({ txtPassword, setTxtPassword }) {
           },
         }
       );
-      const successMessage = response.data;
+      const successMessage = response.data.message;
       Swal.fire({
         icon: "success",
         title: "Cambio Exitoso!",
@@ -57,7 +57,7 @@ export default function UpdatePass({ txtPassword, setTxtPassword }) {
         color: "#000",
         showConfirmButton: false,
         confirmButtonColor: "#000",
-        timer: 1000,
+        timer: 2000,
       });
       handleDelete(); // Llamar a handleDelete después de un cambio exitoso
     } catch (error) {
@@ -65,7 +65,6 @@ export default function UpdatePass({ txtPassword, setTxtPassword }) {
       Swal.fire({
         icon: "error",
         title: messageError,
-        text: error,
         iconColor: "#ff0000",
         color: "#000",
         showConfirmButton: false,
